d9211d4bc5433c51747572dc66c0769a22e8f0dd
Ignacio Loayza <loayzajoseignacio3@gmail.com>
2024-12-03 16:14:02 +0100

    Addind date for db insert

--- clients/src/main/java/aws/connection/ClientDB.java
+++ clients/src/main/java/aws/connection/ClientDB.java
@@ -5,13 +5,15 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ClientDB {
 
-    private static final String NOM_BASE_DE_DADES = "XXX";
-    private static final String URL = "jdbc:postgresql://localhost:5432/" + NOM_BASE_DE_DADES;
-    private static final String USER = "root";
-    private static final String PASSWORD = "XXXXXX";
+    private static final String NOM_BASE_DE_DADES = "sistemas_ciclos";
+    private static final String URL = "jdbc:postgresql://192.168.34.100:5432/" + NOM_BASE_DE_DADES;
+    private static final String USER = "adminitic";
+    private static final String PASSWORD = "admin";
     private static Connection conn = null;
 
     public static void connect() throws SQLException {
@@ -26,7 +28,7 @@ public class ClientDB {
     }
 
     public static boolean isValidUid(String uid) throws SQLException {
-    String queryUid = "SELECT COUNT(*) FROM Usuarios WHERE uid = ?";
+    String queryUid = "SELECT COUNT(*) FROM usuarios WHERE uid = ?";
     
     // Manejo automático de recursos con try-with-resources
     try (PreparedStatement ps = conn.prepareStatement(queryUid)) {
@@ -45,13 +47,14 @@ public class ClientDB {
 
     public static int addUID(String uid) throws SQLException {
     // Usa una consulta parametrizada para evitar inyecciones SQL
-    String sql = "INSERT INTO alumno (UID) VALUES (?)";
+    String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    String sql = "INSERT INTO fecha (uid, horario) VALUES (?, ?)";
 
     // Manejo automático de recursos con try-with-resources
     try (PreparedStatement ps = conn.prepareStatement(sql)) {
         // Asignar parámetro
         ps.setString(1, uid);
-
+        ps.setString(2, date);
         // Ejecutar la consulta y devolver filas afectadas
         return ps.executeUpdate();
         }
